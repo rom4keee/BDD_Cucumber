4cfcc3e66b2c17c36056322ea168782845067169
rom4keee <Hachepaka123>
2022-10-29 16:25:13 +0300

    HW

---
+++ src/main/java/Driver/WDriver.java
@@ -0,0 +1,42 @@
+package Driver;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public class WDriver {
+    private static final ThreadLocal<WebDriver> webDriverThreadLocal = new ThreadLocal<>();
+    private static final long WAIT_TIMEOUT = 5;
+    private static final long PAGE_TIMEOUT = 20;
+
+    private WDriver() {
+    }
+    public static void getWebDriverInstance() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window();
+        driver.manage().timeouts().implicitlyWait(WAIT_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(PAGE_TIMEOUT, TimeUnit.SECONDS);
+        webDriverThreadLocal.set(driver);
+    }
+
+    public static WebDriver getDriver() {
+        return webDriverThreadLocal.get();
+    }
+
+    public static void quitDriver() {
+        Optional.ofNullable(getDriver()).ifPresent(webDriver -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            webDriver.quit();
+            webDriverThreadLocal.remove();
+        });
+    }
+}
